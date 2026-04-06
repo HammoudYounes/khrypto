8daad042981fdfa8c10705feb071bfb028b2d641
youssefBM2104 <mzoughia2104@gmail.com>
2026-04-06 18:31:43 +0200

    chore: expand and refine the swear word filter list for improved content moderation

--- services/social/utils.js
+++ services/social/utils.js
@@ -1,13 +1,20 @@
 const swearWords = [
     // English
-    'fuck', 'shit', 'ass', 'bitch', 'bastard', 'cunt', 'dick',
-    'piss', 'damn', 'crap', 'whore', 'slut', 'prick', 'wank',
-    'twat', 'bollocks', 'cock', 'motherfucker', 'asshole', 'arsehole',
-    // French
-    'putain', 'merde', 'connard', 'connasse', 'salope', 'salopard',
-    'con', 'conne', 'bite', 'cul', 'bordel', 'pute', 'foutre',
-    'encule', 'batard', 'couille', 'couilles', 'chier', 'niquer',
-    'nique', 'fdp', 'tg', 'va-te-faire-foutre'
+    'fuck', 'fucker', 'fucking', 'motherfucker', 'shit', 'bullshit', 'shitty', 'shite', 
+    'ass', 'asshole', 'arsehole', 'asshat', 'asswipe', 'dumbass', 'jackass',
+    'bitch', 'bitching', 'bastard', 'cunt', 'dick', 'dickhead', 'piss', 'damn', 'crap', 
+    'whore', 'slut', 'prick', 'wank', 'wanker', 'twat', 'bollocks', 'cock', 'cocksucker',
+    'faggot', 'fagot', 'pussy', 'cum', 'cumshot', 'dyke', 'dipshit', 
+    'nigger', 'nigga', 'tits', 'tit', 'moron', 'retard',
+    
+    // French (including regional / slang)
+    'putain', 'merde', 'connard', 'connasse', 'salope', 'salopard', 'salaud',
+    'con', 'conne', 'bite', 'cul', 'trouduc', 'trou-du-cul', 'fion', 'bordel', 
+    'pute', 'pouffiasse', 'foutre', 'encule', 'enculé', 'enculée', 'batard', 'bâtard',
+    'couille', 'couilles', 'chier', 'niquer', 'nique', 'niquons', 'baiser', 'baise', 
+    'fdp', 'tg', 'ta gueule', 'va-te-faire-foutre', 'chienne', 'garce', 'gouine', 
+    'pede', 'pédé', 'tapette', 'raclure', 'taré',
+    'zabazoubi', 'zabroun', 'zebi', 'zabzoub', 'tahan'
 ];
 
 const swearPattern = new RegExp(`\\b(${swearWords.join('|')})\\b`, 'gi');
