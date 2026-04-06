3768e066c0ce46d4daa3804e53a1a9ce64e27165
youssefBM2104 <mzoughia2104@gmail.com>
2026-04-06 16:28:25 +0200

    chore: comment out test inventory deletion in auth service

--- services/auth/index.js
+++ services/auth/index.js
@@ -76,8 +76,8 @@ async function displayDatabaseInfo() {
     );
 
     // Wipe dedlix's inventory for testing
-    const inventory_collection = khryto_db.collection('inventory');
-    await inventory_collection.deleteMany({ userId: '69a4bfc60ce2bfc249d25e80' });
+    //const inventory_collection = khryto_db.collection('inventory');
+    //await inventory_collection.deleteMany({ userId: '69a4bfc60ce2bfc249d25e80' });
 
     // 2. See all users in the collection with clear formatting
     const allUsers = await user_collection.find({}).toArray();
